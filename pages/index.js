5c5f01529fce6b1e8faefe8e8951e471e2adf295
Svmpankaj <svmpankaj@gmail.com>
2023-05-23 23:22:11 +0530

    level components and import css file

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Script from 'next/script'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+// import styles1 from '../styles/Home1.module.css'
+// import styles2 from '../styles/Home2.module.css'
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
         <meta name="keyword" content="nextjs, huntingcoder blog, hunting coder" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* <Script src='/sc.js' strategy='lazyOnload'></Script> */}
       <nav className={styles.mainnav}>
         <ul>
           <Link href='/'><li>Home</li> </Link>
@@ -32,7 +35,8 @@ export default function Home() {
           A blog for hunting coders by a hunting coder
         </p>
 
-        <div className="blogs">
+        {/* <div className={`${styles1.con}${styles2.con}`}> */}
+        <div>
           <div className="blogItem">
             <h2>How to learn JavaScript in 2023?</h2>
             <p>JavaScript is the language used to design logic for the web</p>
