9879e0d1fe4f21fea92c2bc4f7c2ddf8688c01af
Svmpankaj <svmpankaj@gmail.com>
2023-05-28 23:44:45 +0530

    using the api endpoints

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
 // Step 1: Collect all the files from blogdata directory
 // Step 2: Iterate through them and Display them 
 const blog = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        console.log("useEffect is running");
+        fetch('http://localhost:3000/api/blogs').then((a) => {
+            return a.json();
+        })
+            .then((parsed) => {
+                console.log(parsed)
+                setBlogs(parsed)
+            })
+    }, [])
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div >
-                    <Link href={'/blogpost/learn-javascript'}>
-                        <h3 className={styles.blogItemh3}>How to learn JavaScript in 2023?</h3></Link>
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
-                <div className="blogItem">
-                    <h3>How to learn JavaScript in 2023?</h3>
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
-                <div className="blogItem">
-                    <h3>How to learn JavaScript in 2023?</h3>
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
+                {blogs.map((blogItem) => {
+                    return <div key={blogItem.title} >
+                        <Link href={'/blogpost/learn-javascript'}>
+                            <h3 className={styles.blogItemh3}>{blogItem.title}</h3></Link>
+                        <p>JavaScript is the language used to design logic for the web</p>
+                    </div>
+                })}
             </main>
         </div>
     )
