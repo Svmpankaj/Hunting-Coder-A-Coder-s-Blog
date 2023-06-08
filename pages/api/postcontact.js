2ab8bcf529115cf461a25a67298e7ed4e99d64d2
Svmpankaj <svmpankaj@gmail.com>
2023-06-08 23:36:58 +0530

    fixed some issue at api

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
         // Process a POST request
-        res.status(200).json(["yes post request"])
+        console.log(req.body)
+        res.status(200).json(req)
     } else {
         // Handler any other HTTP method
         res.status(200).json(["allBlogs"])
