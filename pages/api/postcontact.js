d69cc052b1a7de5781720748d50627f2f16ca211
Svmpankaj <svmpankaj@gmail.com>
2023-06-09 17:48:02 +0530

    creating an api to save contact data

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -1,7 +1,12 @@
-export default function handler(req, res) {
+import * as fs from 'fs';
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         // Process a POST request
         console.log(req.body)
+        let data = await fs.promises.readdir('contactdata');
+        console.log(data)
+        fs.promises.writeFile(`contactdata/${data.length + 1}.json`, JSON.stringify(req.body), () => { })
         res.status(200).json(req)
     } else {
         // Handler any other HTTP method
