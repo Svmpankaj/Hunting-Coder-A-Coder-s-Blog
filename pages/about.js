3f9416a2f8cddeb84e1ede6a7e1e2229224f96bb
Svmpankaj <svmpankaj@gmail.com>
2023-06-13 21:03:04 +0530

    adding about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 function about() {
     return (
-        <div>
-            Hello about how are you
+        <div className={styles.container}>
+            <h1 className={styles.center}>About Hunting Coder</h1>
+            <h2>Introduction</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum in aspernatur officia unde, earum iure ipsam soluta dolores numquam nulla vel nemo. Error, temporibus facere repellat corporis est laborum, voluptatem, atque eum beatae perferendis illo quae reiciendis tempora reprehenderit doloribus provident. Veniam architecto distinctio natus est itaque reprehenderit harum commodi. Sed dignissimos consectetur vel soluta ipsum eveniet? Iste est a deserunt voluptate repudiandae sit magnam </p>
+            <h2>Services Offered</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt quam, ipsum obcaecati voluptatum accusamus aperiam blanditiis consequuntur minima fugiat. A tenetur sit nam blanditiis tempora soluta, et, quia deserunt maxime doloremque mollitia quisquam beatae?</p>
+            <p>We offer the following services</p>
+            <ul>
+                <li>Services 1</li>
+                <li>Services 2</li>
+                <li>Services 3</li>
+                <li>Services 4</li>
+                <li>Services 5</li>
+            </ul>
+            <h2>Contact Us</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi quam repellat officia facere vel hic sed perferendis assumenda excepturi iusto. Consequuntur, fugiat exercitationem ex eveniet dolores explicabo minima error quos? Harum quaerat expedita atque dolorem. Odio, atque quasi. Neque earum dolore, vero amet quos sapiente nam ullam deleniti corrupti error, at maiores mollitia facilis eos esse?</p>
         </div>
     )
 }
