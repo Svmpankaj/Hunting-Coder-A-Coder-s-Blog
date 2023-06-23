7d44c3bf9881f4df8e78dbb47a9b2ccfe3b658e7
Svmpankaj <svmpankaj@gmail.com>
2023-06-23 12:20:47 +0530

    hunting coder is completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import '../styles/globals.css'
 import Navbar from '../components/Navbar'
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <>
-    <Navbar />
-    <Component {...pageProps} />
+  <Navbar/>
+  <Component {...pageProps} />
   </>
 }
+
+export default MyApp
--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 import styles from '../styles/About.module.css'
 
-function about() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.center}>About Hunting Coder</h1>
-            <h2>Introduction</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum in aspernatur officia unde, earum iure ipsam soluta dolores numquam nulla vel nemo. Error, temporibus facere repellat corporis est laborum, voluptatem, atque eum beatae perferendis illo quae reiciendis tempora reprehenderit doloribus provident. Veniam architecto distinctio natus est itaque reprehenderit harum commodi. Sed dignissimos consectetur vel soluta ipsum eveniet? Iste est a deserunt voluptate repudiandae sit magnam </p>
-            <h2>Services Offered</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt quam, ipsum obcaecati voluptatum accusamus aperiam blanditiis consequuntur minima fugiat. A tenetur sit nam blanditiis tempora soluta, et, quia deserunt maxime doloremque mollitia quisquam beatae?</p>
-            <p>We offer the following services</p>
-            <ul>
-                <li>Services 1</li>
-                <li>Services 2</li>
-                <li>Services 3</li>
-                <li>Services 4</li>
-                <li>Services 5</li>
-                <li>Services 6</li>
-                <li>Services 7</li>
-                <li>Services 8</li>
-            </ul>
-            <h2>Contact Us</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi quam repellat officia facere vel hic sed perferendis assumenda excepturi iusto. Consequuntur, fugiat exercitationem ex eveniet dolores explicabo minima error quos? Harum quaerat expedita atque dolorem. Odio, atque quasi. Neque earum dolore, vero amet quos sapiente nam ullam deleniti corrupti error, at maiores mollitia facilis eos esse?</p>
-        </div>
-    )
-}
+const About = () => {
+  return <div className={styles.container}>
+    <h1 className={styles.center}>About Hunting Coder</h1>
+    <h2>Introduction</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quidem? Minus, quas! Earum maxime dignissimos rem quisquam omnis voluptatem ab quis. Tempora iure iste asperiores dolores quia eligendi! Doloremque repellat repellendus deleniti est. Blanditiis officiis et explicabo dicta. Quod, blanditiis at similique enim deserunt labore nisi doloribus tempore nihil quam omnis magni ducimus natus sit vel, id reprehenderit nam non quo consectetur ipsa molestias nobis. </p>
+    <h2>Services Offered</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quis. Saepe, blanditiis harum cum excepturi maiores atque mollitia quas itaque qui, exercitationem id quaerat magnam repellat quia fugiat hic iure sit vitae officiis eligendi.</p>
+    <p>We offer the following services</p>
+    <ul>
+      <li>Service 1</li>
+      <li>Service 2</li>
+      <li>Service 3</li>
+      <li>Service 4</li>
+      <li>Service 5</li>
+      <li>Service 6</li>
+      <li>Service 7</li>
+      <li>Service 8</li>
+    </ul>
+    <h2>Contact us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos et animi harum assumenda necessitatibus explicabo nam, corporis deleniti, ab ad praesentium blanditiis iste saepe perferendis impedit architecto quia cupiditate? Accusantium est in inventore repellendus unde. Dolores labore deserunt corrupti fugiat ratione perspiciatis illo optio suscipit aliquid cumque nihil soluta earum, assumenda eligendi nulla sed molestias unde?</p>
+  </div>;
+};
 
-export default about
+export default About;
